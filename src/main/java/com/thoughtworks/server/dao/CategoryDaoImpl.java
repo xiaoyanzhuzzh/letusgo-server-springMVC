e75265f964dd9d6bfbb99c77e095f22017a0cf39
zzh <xiaoyanzhuzzh@sina.com>
2014-11-10 22:34:01 +0800

    implement CategoryDao interface

--- src/main/java/com/thoughtworks/server/dao/CategoryDaoImpl.java
+++ src/main/java/com/thoughtworks/server/dao/CategoryDaoImpl.java
@@ -1,7 +1,32 @@
 package com.thoughtworks.server.dao;
 
-/**
- * Created by zhangzhihui on 14-11-10.
- */
-public class CategoryDaoImpl {
+import com.thoughtworks.server.model.Category;
+
+import java.util.List;
+
+public class CategoryDaoImpl implements CategoryDao{
+    @Override
+    public Category getCategoryById(int id) {
+        return null;
+    }
+
+    @Override
+    public void insertCategory(Category category) {
+
+    }
+
+    @Override
+    public void deleteCategoryById(int id) {
+
+    }
+
+    @Override
+    public void updateCategoryById(int id) {
+
+    }
+
+    @Override
+    public List<Category> getCategories() {
+        return null;
+    }
 }
