5b0b09dcc949ebf12fa60c43ca1ec171b0ace145
zzh <xiaoyanzhuzzh@sina.com>
2014-11-10 22:39:43 +0800

    finish getCategoryById method

--- src/main/java/com/thoughtworks/server/dao/CategoryDaoImpl.java
+++ src/main/java/com/thoughtworks/server/dao/CategoryDaoImpl.java
@@ -1,13 +1,30 @@
 package com.thoughtworks.server.dao;
 
 import com.thoughtworks.server.model.Category;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+@Repository
 public class CategoryDaoImpl implements CategoryDao{
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
     @Override
     public Category getCategoryById(int id) {
-        return null;
+        String sql = "SELECT * FROM categories WHERE id = ?";
+        return jdbcTemplate.queryForObject(sql, new RowMapper<Category>() {
+            @Override
+            public Category mapRow(ResultSet rs, int i) throws SQLException {
+                return new Category(rs.getInt("id"), rs.getString("name"), rs.getInt("numOfItem"));
+            }
+        });
     }
 
     @Override
