42ded6e71a44c961612d9c8666d456e32a90db4d
zzh <xiaoyanzhuzzh@sina.com>
2014-11-10 22:19:59 +0800

    finish Category Class

--- src/main/java/com/thoughtworks/server/model/Category.java
+++ src/main/java/com/thoughtworks/server/model/Category.java
@@ -1,7 +1,37 @@
 package com.thoughtworks.server.model;
 
-/**
- * Created by zhangzhihui on 14-11-10.
- */
 public class Category {
+    private int id;
+    private String name;
+    private int numberOfItem;
+
+    public Category(int id, String name, int numberOfItem) {
+        this.id = id;
+        this.name = name;
+        this.numberOfItem = numberOfItem;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getNumberOfItem() {
+        return numberOfItem;
+    }
+
+    public void setNumberOfItem(int numberOfItem) {
+        this.numberOfItem = numberOfItem;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
