2b51dce44e7163599e455b7bc102ec4ebe051f1d
zzh <xiaoyanzhuzzh@sina.com>
2014-11-10 21:50:46 +0800

    finish create data in mysql

--- src/main/resources/create.sql
+++ src/main/resources/create.sql
@@ -1,62 +1,46 @@
-use management;
+DROP DATABASE EXISTS server-java;
+CREATE DATABASE server-java DEFAULT CHARACTER SET utf8;
+use server-java;
 
-drop table items_promotions;
-drop table items;
-drop table promotions;
 drop table categories;
- 
-create table items (
-  id int(11) NOT NULL auto_increment,
-  barcode varchar(30) NOT NULL,
-  name varchar(30) NOT NULL,
-  unit varchar(4) NOT NULL,
-  price double NOT NULL,
-  categoryId int(11) default NULL,
-  PRIMARY KEY  (id)
-);
+drop table items;
+drop table cartItems;
 
-create table promotions(
-   id int not null auto_increment,
-   proDesc varchar(200),
-   type int not null,
-   primary key(id)
-);
-
-create table items_promotions(
-   itemId int not null references items(id),
-   proId int not null references promotions(id),
-   discount double,
-   primary key(itemId,proId),
-   foreign key(itemId) references items(id),
-   foreign key(proId) references promotions(id)
-);
 create table categories (
   id int(11) NOT NULL auto_increment,
   name varchar(30) NOT NULL,
+  num int(11) NOT NULL,
   PRIMARY KEY  (id)
 );
-alter table items add constraint cate_item_fk foreign key(categoryId) references categories(id);
-insert into categories values(null,'水果');
-insert into categories values(null,'饮料');
-insert into categories values(null,'运动器材');
 
-insert into items values(null,'ITEM000001','葡萄','斤',6.5,1);
-insert into items values(null,'ITEM000002','橙汁','瓶',3.5,2);
-insert into items values(null,'ITEM000003','苹果','斤',2.5,1);
-insert into items values(null,'ITEM000004','羽毛球','个',1.0,3);
-insert into items values(null,'ITEM000005','可口可乐','瓶',4.0,2);
+create table items (
+  id int(11) NOT NULL auto_increment,
+  name varchar(30) NOT NULL,
+  price double NOT NULL,
+  unit varchar(4) NOT NULL,
+  categoryId int(11) NOT NULL,
+  PRIMARY KEY (id),
+  foreign key(categoryId) references categories(id)
+);
 
-insert into promotions values(null,'买二赠一', 1);
-insert into promotions values(null,'第二件半价', 2);
-insert into promotions values(null,'打折', 3);
+create table cartItems (
+  id int(11) NOT NULL auto_increment,
+  itemId int(11) NOT NULL,
+  num int(11) NOT NULL,
+  PRIMARY KEY (id),
+  foreign key(itemId) references items(id)
+);
 
-insert into items_promotions values(1,1,1.0);
-insert into items_promotions values(1,2,1.0);
-insert into items_promotions values(1,3,0.65);
-insert into items_promotions values(3,1,1.0);
-insert into items_promotions values(3,2,1.0);
-insert into items_promotions values(3,3,0.70);
-insert into items_promotions values(5,1,1.0);
-insert into items_promotions values(5,2,1.0);
-insert into items_promotions values(5,3,0.75);
+insert into categories values(null,'饮品', 0);
+insert into categories values(null,'水果', 0);
+insert into categories values(null,'零食', 0);
+insert into categories values(null,'生活用品', 0);
 
+insert into items values(null, '葡萄', 6.5, '斤', 2);
+insert into items values(null, '雪碧', 3.5, '瓶', 1);
+insert into items values(null, '可口可乐', 3.0, '瓶', 1);
+insert into items values(null, '方便面', 4.0, '袋', 3);
+insert into items values(null, '电池', 2.0, '个', 4);
+insert into items values(null, '苹果', 3.5, '斤', 2);
+
+COMMIT;
