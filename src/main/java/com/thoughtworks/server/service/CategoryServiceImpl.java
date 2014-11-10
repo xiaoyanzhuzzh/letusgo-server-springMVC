e44048c65fd006451ba06de529a867737bf8007e
zzh <xiaoyanzhuzzh@sina.com>
2014-11-10 22:44:34 +0800

    finish getCategoryById method

--- src/main/java/com/thoughtworks/server/service/CategoryServiceImpl.java
+++ src/main/java/com/thoughtworks/server/service/CategoryServiceImpl.java
@@ -1,7 +1,40 @@
 package com.thoughtworks.server.service;
 
-/**
- * Created by zhangzhihui on 14-11-10.
- */
-public class CategoryServiceImpl {
+import com.thoughtworks.server.dao.CategoryDao;
+import com.thoughtworks.server.model.Category;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryServiceImpl implements CategoryService{
+
+    @Autowired
+    private CategoryDao categoryDaoImpl;
+
+    @Override
+    public Category getCategoryById(int id) {
+        return categoryDaoImpl.getCategoryById(id);
+    }
+
+    @Override
+    public void insertCategory(Category category) {
+
+    }
+
+    @Override
+    public void deleteCategoryById(int id) {
+
+    }
+
+    @Override
+    public void updateCategoryById(int id) {
+
+    }
+
+    @Override
+    public List<Category> getCategories() {
+        return null;
+    }
 }
