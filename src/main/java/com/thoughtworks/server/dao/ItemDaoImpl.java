f4dc0e667bdc099e6572335f75c42d167a5835bb
zzh <xiaoyanzhuzzh@sina.com>
2014-11-12 00:18:57 +0800

    add annotation @repository

--- src/main/java/com/thoughtworks/server/dao/ItemDaoImpl.java
+++ src/main/java/com/thoughtworks/server/dao/ItemDaoImpl.java
@@ -1,7 +1,9 @@
 package com.thoughtworks.server.dao;
 
 import com.thoughtworks.server.model.Item;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class ItemDaoImpl implements ItemDao{
     @Override
     public Item getItemById(int id) {
