b26a808d5ec05ca557584ddfb450710d2d426917
zzh <xiaoyanzhuzzh@sina.com>
2014-11-11 19:36:11 +0800

    finish mock_CategoryDaoImpl

--- src/test/java/com/thoughtworks/server/service/CategoryServiceImplTest.java
+++ src/test/java/com/thoughtworks/server/service/CategoryServiceImplTest.java
@@ -1,7 +1,32 @@
 package com.thoughtworks.server.service;
 
-/**
- * Created by zhangzhihui on 14-11-11.
- */
+import com.thoughtworks.server.dao.CategoryDao;
+import com.thoughtworks.server.dao.CategoryDaoImpl;
+import com.thoughtworks.server.model.Category;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 public class CategoryServiceImplTest {
+    @Before
+    public void mock_CategoryDaoImpl(){
+        CategoryDao categoryDaoImpl = mock(CategoryDaoImpl.class);
+        int id = 1;
+        Category category = new Category(1, "水果", 2);
+        when(categoryDaoImpl.getCategoryById(id)).thenReturn(category);
+
+        CategoryService categoryServiceImpl = new CategoryServiceImpl();
+        categoryServiceImpl.setCategoryDaoImpl(categoryDaoImpl);
+    }
+
+
+
+    @Test
+    public void can_get_category_by_id(){
+    }
+
 }
