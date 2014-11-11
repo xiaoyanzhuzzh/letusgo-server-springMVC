21bcf235aa4b3742f420dfdf20877da3e60502b4
zzh <xiaoyanzhuzzh@sina.com>
2014-11-12 00:23:38 +0800

    finish getItemById in dao

--- src/main/java/com/thoughtworks/server/dao/ItemDaoImpl.java
+++ src/main/java/com/thoughtworks/server/dao/ItemDaoImpl.java
@@ -1,12 +1,29 @@
 package com.thoughtworks.server.dao;
 
 import com.thoughtworks.server.model.Item;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @Repository
 public class ItemDaoImpl implements ItemDao{
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
     @Override
     public Item getItemById(int id) {
-        return null;
+        final String sql = "SELECT * FROM items WHERE id = ?";
+
+        return jdbcTemplate.queryForObject(sql, new RowMapper<Item>() {
+            @Override
+            public Item mapRow(ResultSet rs, int i) throws SQLException {
+                return new Item(rs.getInt("id"), rs.getString("name"), rs.getDouble("price"),
+                        rs.getString("unit"), rs.getInt("categoryId"));
+            }
+        }, id);
     }
 }
