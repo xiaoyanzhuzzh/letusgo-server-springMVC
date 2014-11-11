9b30559f48a833c21f266ade3f51dc7809714244
zzh <xiaoyanzhuzzh@sina.com>
2014-11-12 00:28:44 +0800

    finish getItemById in controller

--- src/main/java/com/thoughtworks/server/controller/ItemController.java
+++ src/main/java/com/thoughtworks/server/controller/ItemController.java
@@ -1,7 +1,18 @@
 package com.thoughtworks.server.controller;
 
-/**
- * Created by zhangzhihui on 14-11-12.
- */
+import com.thoughtworks.server.model.Item;
+import com.thoughtworks.server.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
 public class ItemController {
+    @Autowired
+    private ItemService itemServiceImpl;
+
+    @RequestMapping(value = "/items/{id}", method = RequestMethod.GET)
+    public @ResponseBody Item getItemById(@PathVariable int id){
+        return itemServiceImpl. getItemById(id);
+    }
 }
